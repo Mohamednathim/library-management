b73927ee517d7caae55c309500f0258372da164c
Mohamednathim <mohamednathim84@gmail.com>
2024-01-22 20:03:31 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link } from "react-router-dom";
+import AddBooks from "./components/Books/AddBooks";
+import AddAuthorDetails from "./components/Author/AddAuthorDetails";
+import GetBooks from "./components/Books/GetBooks";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import GetAuthors from "./components/Author/GetAuthor";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="d-flex flex-start px-2">
+          <h2> Library...📚</h2>
+        </div>
+        <div className="navbarHeading">
+          <ul className="navbar-nav">
+            <li className="nav-item px-2">
+              <Link className="nav-link" to="/">
+                <h4>Home</h4>
+              </Link>
+            </li>
+            <li className="nav-item px-3">
+              <Link className="nav-link" to="/Books">
+                <h4>Books</h4>
+              </Link>
+            </li>
+            <li className="nav-item px-2">
+              <Link className="nav-link" to="/Author">
+                <h4>Author</h4>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Books" element={<GetBooks />}></Route>
+        <Route path="/Books/:id" element={<AddBooks />}></Route>
+        <Route path="/Author" element={<GetAuthors />}></Route>
+        <Route path="/Author/:id" element={<AddAuthorDetails />}></Route>
+      </Routes>
+    </>
   );
 }
 
